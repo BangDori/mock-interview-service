111db5499f28842c3ec58105fcd836aa3189ee25
BangDori <qudwns8865@naver.com>
2023-12-16 22:41:42 +0900

    feat: 면접 시간 기능 추가

--- src/components/Interview.tsx
+++ src/components/Interview.tsx
@@ -1,13 +1,30 @@
-import { useState } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 import nextArrowBtn from '@assets/next_arrow_button.png';
 import { Question } from '@custom/types/Question';
 import '@styles/InterviewContainer.scss';
 
+function convertNumberToTimeStamp(timer: number) {
+  const hour: string = (timer / 60).toFixed().padStart(2, '0');
+  const min: string = (timer % 60).toString().padStart(2, '0');
+
+  return `${hour}:${min}`;
+}
+
+const timeStamps = [];
+
 const Interview: React.FC<{ questions: Question[] }> = ({ questions }) => {
   const [id, setId] = useState<number>(0);
+  const [timer, setTimer] = useState<number>(0);
+  const timerRef = useRef<number | null>(null);
   const navigate = useNavigate();
+
+  useEffect(() => {
+    timerRef.current = setInterval(() => {
+      setTimer((prevTimer) => prevTimer + 1);
+    }, 1000) as unknown as number;
+  }, [id]);
 
   const nextQuestion = () => {
     if (id === questions.length - 1) {
@@ -16,10 +33,18 @@ const Interview: React.FC<{ questions: Question[] }> = ({ questions }) => {
     }
 
     setId((prevId) => prevId + 1);
+    timeStamps.push(timer);
+    setTimer(0);
+    if (timerRef.current !== null) {
+      clearInterval(timerRef.current);
+    }
   };
 
   return (
     <div className='interview-container'>
+      <div className='timer-box'>
+        <h3 className='timer'>{convertNumberToTimeStamp(timer)}</h3>
+      </div>
       <div className='question-box'>
         <p className='question'>{questions[id].text}</p>
         <button type='button' className='next-arrow-btn' onClick={nextQuestion}>
